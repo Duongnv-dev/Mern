ea43daf23856c1dc07cd002f83ee11a42e866803
DuongNv2 <duongnv2@rikkeisoft.com>
2022-06-07 17:38:29 +0700

    validate register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,11 @@ import React, { Fragment, useState } from "react";
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom"
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ( {setAlert} ) => {
+const Register = ( {setAlert, register} ) => {
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -15,13 +17,13 @@ const Register = ( {setAlert} ) => {
   const { name, email, password, password2 } = formData;
 
   const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
-
+  
   const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2) {
       setAlert('Password does not match', 'danger');
     } else {
-      console.log('Success');
+      register({ name, email, password });
     }
   }
 
@@ -39,7 +41,6 @@ const Register = ( {setAlert} ) => {
             name="name"
             value={name}
             onChange={(e) => onChange(e)}
-            required
           />
         </div>
         <div className="form-group">
@@ -49,7 +50,6 @@ const Register = ( {setAlert} ) => {
             name="email"
             value={email}
             onChange={(e) => onChange(e)}
-            required
           />
           <small className="form-text">
             This site uses Gravatar, so if you want a profile image, use a
@@ -84,9 +84,10 @@ const Register = ( {setAlert} ) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired, 
+  register: PropTypes.func.isRequired, 
 }
 
 export default connect(null, 
-  { setAlert }
+  { setAlert, register }
   )(Register);
